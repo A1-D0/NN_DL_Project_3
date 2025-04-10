e546b728a5fcfe72ab59673a9c892b049aea6b50
rocaaj <97640350+rocaaj@users.noreply.github.com>
2025-04-10 18:06:00 -0400

    fixed env issues

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,20 +5,16 @@
 
 import cv2
 import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing import image
+from keras.utils import load_img, img_to_array
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 import numpy as np
 
 
 def load_image_for_resnet(img_path):
-    """
-    Load and preprocess an image for ResNet50.
-    """
-    img = image.load_img(img_path, target_size=(224, 224))
-    x = image.img_to_array(img)
+    img = load_img(img_path, target_size=(224, 224))
+    x = img_to_array(img)
     x = np.expand_dims(x, axis=0)
-    from tensorflow.keras.applications.resnet50 import preprocess_input
     return preprocess_input(x)
-
 
 def show_image(img_path, title="Image"):
     """
