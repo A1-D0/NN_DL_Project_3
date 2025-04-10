dad38c06a312dd091067c3dcd27d01f5d7e90e94
rocaaj <97640350+rocaaj@users.noreply.github.com>
2025-04-10 18:15:29 -0400

    added example code for modular .py file pipeline

--- scripts/main.py
+++ scripts/main.py
@@ -4,9 +4,9 @@
 # -------------------------
 
 import os
-from utils import show_image
-from yolo_mod import load_yolov3_model, run_yolov3_inference
-from resnet_mod import load_resnet50_model, run_resnet50_inference
+import utils
+import yolo_mod as yolo
+import resnet_mod as resnet
 
 
 # Constants
@@ -16,17 +16,17 @@ TEST_IMAGE = os.path.join(DATASET_DIR, 'images/0000001_00000_d_0000001.jpg')
 
 def main():
     # Display the sample image
-    show_image(TEST_IMAGE, "Sample Image")
+    utils.show_image(TEST_IMAGE, "Sample Image")
 
     # YOLOv3 Inference
     print("\n--- YOLOv3 Results ---")
-    yolov3_model = load_yolov3_model()
-    yolov3_results = run_yolov3_inference(yolov3_model, TEST_IMAGE)
+    yolov3_model = yolo.load_yolov3_model()
+    yolov3_results = yolo.run_yolov3_inference(yolov3_model, TEST_IMAGE)
 
     # ResNet50 Inference
     print("\n--- ResNet50 Results ---")
-    resnet_model = load_resnet50_model()
-    resnet_results = run_resnet50_inference(resnet_model, TEST_IMAGE)
+    resnet_model = resnet.load_resnet50_model()
+    resnet_results = resnet.run_resnet50_inference(resnet_model, TEST_IMAGE)
     for idx, (cls, label, prob) in enumerate(resnet_results):
         print(f"{idx+1}. {label}: {prob:.4f}")
 
